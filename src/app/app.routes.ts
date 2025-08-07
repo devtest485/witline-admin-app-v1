7771a658bad510145e4390a06f0fe3c67203954d
Faizan Hamza <devtest485@gmail.com>
2025-08-07 18:56:30 +0500

    fixed layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,24 +10,31 @@ import { WebComponentComponent } from '../main-panel/shared/components/data-disp
 import { ModelViewComponent } from '../main-panel/shared/components/data-display/model-view/model-view.component';
 import { ModelFieldComponent } from '../main-panel/shared/components/data-display/model-field/model-field.component';
 import { PreComponentComponent } from '../main-panel/shared/components/data-display/pre-component/pre-component.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'grid-view', component: GridViewComponent },
-  { path: 'list-view', component: ListViewComponent },
-  { path: 'panel-view', component: PanelViewComponent },
-  { path: 'multi-view', component: MultiViewComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'grid-view', component: GridViewComponent },
+      { path: 'list-view', component: ListViewComponent },
+      { path: 'panel-view', component: PanelViewComponent },
+      { path: 'multi-view', component: MultiViewComponent },
 
-  { path: 'html-block', component: HtmlBlockComponent },
-  { path: 'template', component: TemplateViewComponent },
-  { path: 'web-component', component: WebComponentComponent },
-  { path: 'models', component: ModelViewComponent },
+      { path: 'html-block', component: HtmlBlockComponent },
+      { path: 'template', component: TemplateViewComponent },
+      { path: 'web-component', component: WebComponentComponent },
+      { path: 'models', component: ModelViewComponent },
 
-  { path: 'form-builder', component: MultiViewComponent },
-  { path: 'mode-fields', component: ModelFieldComponent },
-  { path: 'pre-component', component: PreComponentComponent },
+      { path: 'form-builder', component: MultiViewComponent },
+      { path: 'mode-fields', component: ModelFieldComponent },
+      { path: 'pre-component', component: PreComponentComponent },
 
-  { path: 'setting', component: MultiViewComponent },
+      { path: 'setting', component: MultiViewComponent },
+    ]
+  }
 
 ];
